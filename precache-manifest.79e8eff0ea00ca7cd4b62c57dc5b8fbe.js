b87566c687a0f918eb227037862710108682420e
trynantapat009 <110448881+trynantapat009@users.noreply.github.com>
2022-11-11 11:50:44 +0700

    Add files via upload

---
+++ precache-manifest.79e8eff0ea00ca7cd4b62c57dc5b8fbe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "1221213292f079bc0b66",
+    "url": "/static/css/main.bbc84ad3.chunk.css"
+  },
+  {
+    "revision": "1221213292f079bc0b66",
+    "url": "/static/js/main.12212132.chunk.js"
+  },
+  {
+    "revision": "271d1efb744527e0afa6",
+    "url": "/static/js/1.271d1efb.chunk.js"
+  },
+  {
+    "revision": "a18a58a14dc553538bb9",
+    "url": "/static/js/2.a18a58a1.chunk.js"
+  },
+  {
+    "revision": "b1035dd03e2e0c8b5520",
+    "url": "/static/js/runtime~main.b1035dd0.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "c0b32d10a81e792caf17e68d12a2b1db",
+    "url": "/index.html"
+  }
+];
